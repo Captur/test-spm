b78651523f26f6ccca1885a7ea46b6a00df4b2f4
ahd-3hd <ahdhani0@gmail.com>
2025-05-16 13:47:46 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
     .library(
             name: "CapturMicromobilityEvents",
-            targets: ["Test SPM"])   // public name
+            targets: ["CapturMicromobilityEvents"])   // public name
         ],
     targets: [
     .binaryTarget(
